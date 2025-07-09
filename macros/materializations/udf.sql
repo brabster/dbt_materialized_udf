8f5bb4d00b1b386124001419bcfad172355c6f0f
Paul Brabban <paul.brabban@gmail.com>
2025-07-09 15:10:36 +0000

    handle case where returns is not specified; return a relation to deal with suspect dbt Cloud/semantic layer issue when no relation is returned

--- macros/materializations/udf.sql
+++ macros/materializations/udf.sql
@@ -1,13 +1,17 @@
 {% materialization udf, adapter="bigquery" %}
 {%- set target = adapter.quote(this.database ~ '.' ~ this.schema ~ '.' ~ this.identifier) -%}
 
+{%- set target_relation = api.Relation.create(identifier=this.identifier, schema=this.schema, database=this.database) -%}
+
 {%- set parameter_list=config.get('parameter_list') -%}
 {%- set ret=config.get('returns') -%}
 {%- set description=config.get('description') -%}
 
 {%- set create_sql -%}
 CREATE OR REPLACE FUNCTION {{ target }}({{ parameter_list }})
+{%- if ret %}
 RETURNS {{ ret }}
+{%- endif %}
 OPTIONS (
   description='{{ description }}'
 )
@@ -20,6 +24,6 @@ AS (
   {{ create_sql }}
 {%- endcall %}
 
-{{ return({'relations': []}) }}
+{{ return({'relations': [target_relation]}) }}
 
 {% endmaterialization %}
